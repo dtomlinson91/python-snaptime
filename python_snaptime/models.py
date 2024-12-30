bcb0d465b5a26dee875ad7795bc6ec5c2ea5d3fb
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2024-12-30 18:13:23 +0000

    add package

---
+++ python_snaptime/models.py
@@ -0,0 +1,77 @@
+"""Module defining models for python-snaptime."""
+
+from __future__ import annotations
+
+from enum import Enum
+from typing import TypedDict
+
+from pydantic import BaseModel, model_validator
+
+
+class Action(str, Enum):
+    """Time Snapping Actions."""
+
+    SNAP = "@"
+    ADD = "+"
+    SUB = "-"
+
+
+class Unit(Enum):
+    """Time Units."""
+
+    MILLISECOND = "ms", "millisecond", "milliseconds"
+    SECOND = "s", "sec", "secs", "second", "seconds"
+    MINUTE = "m", "min", "mins", "minute", "minutes"
+    HOUR = "h", "hr", "hrs", "hour", "hours"
+    DAY = "d", "day", "days"
+    WEEK = "w", "week", "weeks"
+    MONTH = "mon", "month", "months"
+    QUARTER = "q", "qtr", "qtrs", "quarter", "quarters"
+    YEAR = "y", "yr", "yrs", "year", "years"
+
+    @classmethod
+    def _missing_(cls, value: object):  # noqa: ANN206
+        for member in cls:
+            if value in member.value:
+                return member
+        return None
+
+
+class SnaptimeDict(TypedDict, total=False):
+    """Dictionary representing a snaptime configuration."""
+
+    action: str | None
+    unit: str | None
+    time_int: int | None
+
+
+class Snaptime(BaseModel):
+    """Model representing a snaptime configuration."""
+
+    action: Action | None = None
+    unit: Unit | None = None
+    time_int: int | None = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def _verify_model(cls, values: SnaptimeDict) -> SnaptimeDict:
+        action = values.get("action")
+        unit = values.get("unit")
+        time_int = values.get("time_int")
+
+        if action is None:
+            raise ValueError("Snaptime string is invalid: must provide either a snap `@` or time delta `+-`.")
+
+        if action == Action.SNAP:
+            if time_int is not None:
+                raise ValueError("Snaptime string is invalid: cannot use a time integer when snapping.")
+            if unit is None:
+                raise ValueError("Snaptime string is invalid: missing time unit when snapping.")
+            if unit in Unit.MILLISECOND.value:
+                raise ValueError("Snaptime string is invalid: cannot snap to nearest millisecond.")
+        elif action in (Action.ADD, Action.SUB):
+            if time_int is None:
+                raise ValueError("Snaptime string is invalid: missing time integer for time addition or subtraction.")
+            if unit is None:
+                raise ValueError("Snaptime string is invalid: missing time unit for time addition or subtraction.")
+        return values
