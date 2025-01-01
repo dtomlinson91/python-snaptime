b3aafc2a2fc3b76b9f4cbe51f19d42b017edbc8f
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2025-01-01 22:06:20 +0000

    update tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,60 @@ import re
 
 import pytest
 
-from python_snaptime.models import Snaptime
+from python_snaptime.models import Snaptime, Unit
+
+
+@pytest.mark.parametrize(
+    "unit,result",
+    [
+        ("us", Unit.MICROSECOND),
+        ("microsecond", Unit.MICROSECOND),
+        ("microseconds", Unit.MICROSECOND),
+        ("ms", Unit.MILLISECOND),
+        ("millisecond", Unit.MILLISECOND),
+        ("milliseconds", Unit.MILLISECOND),
+        ("s", Unit.SECOND),
+        ("sec", Unit.SECOND),
+        ("secs", Unit.SECOND),
+        ("second", Unit.SECOND),
+        ("seconds", Unit.SECOND),
+        ("m", Unit.MINUTE),
+        ("min", Unit.MINUTE),
+        ("mins", Unit.MINUTE),
+        ("minute", Unit.MINUTE),
+        ("minutes", Unit.MINUTE),
+        ("h", Unit.HOUR),
+        ("hr", Unit.HOUR),
+        ("hrs", Unit.HOUR),
+        ("hour", Unit.HOUR),
+        ("hours", Unit.HOUR),
+        ("d", Unit.DAY),
+        ("day", Unit.DAY),
+        ("days", Unit.DAY),
+        ("w", Unit.WEEK),
+        ("week", Unit.WEEK),
+        ("weeks", Unit.WEEK),
+        ("mon", Unit.MONTH),
+        ("month", Unit.MONTH),
+        ("months", Unit.MONTH),
+        ("q", Unit.QUARTER),
+        ("qtr", Unit.QUARTER),
+        ("qtrs", Unit.QUARTER),
+        ("quarter", Unit.QUARTER),
+        ("quarters", Unit.QUARTER),
+        ("y", Unit.YEAR),
+        ("yr", Unit.YEAR),
+        ("yrs", Unit.YEAR),
+        ("year", Unit.YEAR),
+        ("years", Unit.YEAR),
+    ],
+)
+def test_microseconds(unit, result):
+    # act
+    result = Unit(unit)
+
+    # assert
+    assert result == result
 
 
 class TestSnaptime:
