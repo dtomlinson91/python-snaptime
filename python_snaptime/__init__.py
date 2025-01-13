56d10db3ea6024b669c33029cda5999cda5a1039
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2025-01-13 00:12:55 +0000

    add advanced methods/models to main import

--- python_snaptime/__init__.py
+++ python_snaptime/__init__.py
@@ -1,5 +1,7 @@
 """Python Snaptime package."""
 
+from python_snaptime.handlers import handle_timesnapping
 from python_snaptime.main import snap
+from python_snaptime.models import Action, Snaptime, Unit
 
-__all__ = ["snap"]
+__all__ = ["Action", "Snaptime", "Unit", "handle_timesnapping", "snap"]
